e258f1d3d3381586bb610b904eb6575c7e84c59f
m-sh77 <mehrnaz17@yahoo.de>
2016-05-22 13:36:58 +0200

    Setting up android studio project

---
+++ app/src/main/java/com/msh77/alephba/AlephaMainActivity.java
@@ -0,0 +1,13 @@
+package com.msh77.alephba;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AlephaMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alepha_main);
+    }
+}
